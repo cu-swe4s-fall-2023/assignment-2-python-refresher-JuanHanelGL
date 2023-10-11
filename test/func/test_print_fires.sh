900952f4680eea03b8c3fae7180e64bc62b843da
JuanHanelGL <juan.hanel@colorado.edu>
2023-10-11 22:25:30 +0000

    Changed yml file5

--- test/func/test_print_fires.sh
+++ test/func/test_print_fires.sh
@@ -17,6 +17,7 @@ assert_exit_code 0
 
 run std_dev_test python $script_path --country "United States of America" --country_column 0 --fires_column 4 --file_name "test_data.csv" --operation 4
 assert_in_stdout 2972.362177722553
+assert_in_stdout 2972.3621777225526
 assert_exit_code 0
 
 
