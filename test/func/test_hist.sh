4d501e37248f5c965efeb604133ae8ae6d63f73f
JuanHanelGL <juan.hanel@colorado.edu>
2023-10-15 19:48:07 +0000

    Added get_data and hist 3

--- test/func/test_hist.sh
+++ test/func/test_hist.sh
@@ -4,11 +4,11 @@ test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest
 
 script_path="../../src/hist.py"
 
-if [ -f "Afghanistan.jpg" ]; then
+if [ -f "Afghanistan.png" ]; then
     rm Afghanistan.jpg
 fi
 
-run get_data_test python $script_path --file_name "Afghanistan.txt" --out_file "Afghanistan.jpg" --title "Food Data" --x "Food_household_consumption" --y "Year"
+run get_data_test python $script_path --file_name "Afghanistan.txt" --out_file "Afghanistan.png" --title "Food Data" --x "Food_household_consumption" --y "Year"
 
 
-assert_equal $"Afghanistan.jpg" $(ls $"Afghanistan.jpg")
+assert_equal $"Afghanistan.png" $(ls $"Afghanistan.png")
